9c9f8e4d77ca7f566f7b4613975a56995ead518f
iBel251 <ibbimran7@gmail.com>
2022-03-29 07:41:27 +0300

    Added headline and javascript file for date functionality.

---
+++ script.js
@@ -0,0 +1,7 @@
+var dt = new Date();
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var currentDate =dt.getDate() +' ' + months[dt.getMonth()] +' '+dt.getFullYear();
+var currentTime = dt.getHours() + ':' + dt.getMinutes();
+document.getElementsByClassName("date")[0].innerHTML=currentDate;
+document.getElementsByClassName("date")[1].innerHTML=currentDate;
+//document.getElementById('time').innerHTML=currentTime;
