fef548745ce2d3f2d4d5d5a09813e99c72022387
iBel251 <ibbimran7@gmail.com>
2022-03-31 10:00:47 +0300

    Add footer and intertainment page

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const footer = document.querySelector('.footer');
+fetch('/footer.html')
+.then(res=>res.text())
+.then(data=>{
+    footer.innerHTML=data
+})
+
+
 var dt = new Date();
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var currentDate =dt.getDate() +' ' + months[dt.getMonth()] +' '+dt.getFullYear();
@@ -5,3 +13,4 @@ var currentTime = dt.getHours() + ':' + dt.getMinutes();
 document.getElementsByClassName("date")[0].innerHTML=currentDate;
 document.getElementsByClassName("date")[1].innerHTML=currentDate;
 //document.getElementById('time').innerHTML=currentTime;
+
